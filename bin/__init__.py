6945ef9b9639860971d001aa46bbdd2e233b4628
zhaiweixi <zhaiweixi@idstaff.com>
2019-08-08 10:03:34 +0800

    train model

--- bin/__init__.py
+++ bin/__init__.py
@@ -17,5 +17,21 @@ import math
 
 from gensim.models import word2vec
 
-import pyspark
+import redis
 
+from rediscluster import StrictRedisCluster
+
+
+import re
+pattern = re.compile(r'(?<![\.\d])(?:\d{1,3}\.){3}\d{1,3}(?![\.\d])')
+pattern2 = re.compile(r'[\u4E00-\u9FA5]+')
+p2 = re.compile(r'[a-zA-Z0-9]+|[\u4E00-\u9FA5]+')
+st = u"hello,world!!%[545]你好 世界。。。"
+ste = re.sub("^[A-Za-z0-9]+", "", st)
+
+new_s = ""
+for s in st:
+    if p2.match(s):
+        new_s += s
+
+print(new_s)
